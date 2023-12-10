c2d2648eb65a40fe33afdababba75f412581c766
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-12-10 23:53:49 +0300

    Server : Category

--- E_Commerce_Server/Program.cs
+++ E_Commerce_Server/Program.cs
@@ -1,3 +1,5 @@
+using E_Commerce_Business.Repository;
+using E_Commerce_Business.Repository.IRepository;
 using E_Commerce_DataAccess.Data;
 using E_Commerce_Server.Data;
 using Microsoft.AspNetCore.Components;
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));//Path
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddRazorPages();
