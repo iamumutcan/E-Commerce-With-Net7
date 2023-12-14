e39d9c7b73a60d3570d400ae896f06de6b804151
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-12-14 20:12:27 +0300

    Client : Added ProductService

--- E_Commerce_API/Program.cs
+++ E_Commerce_API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));//Path
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
