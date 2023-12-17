dff3692d7e9895792dbcc93e602cd955b55c109a
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-12-17 23:07:54 +0300

    DataAccess : Order

--- E_Commerce_DataAccess/Data/ApplicationDbContext.cs
+++ E_Commerce_DataAccess/Data/ApplicationDbContext.cs
@@ -10,7 +10,8 @@ namespace E_Commerce_DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductPrice> ProductPrices { get; set; }
-
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
 
     }
 }
