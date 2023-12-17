3be0cd2af04278bbddf0a330087849f379887971
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-12-17 23:09:26 +0300

    Business : Mapping Order

--- E_Commerce_Business/Mapping/MappingProfile.cs
+++ E_Commerce_Business/Mapping/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using E_Commerce_DataAccess;
+using E_Commerce_DataAccess.Models;
 using E_Commerce_Models;
 
 namespace E_Commerce_Business.Mapping
@@ -11,7 +12,9 @@ namespace E_Commerce_Business.Mapping
             CreateMap<Category, CategoryDTO>().ReverseMap();
             CreateMap<Product, ProductDTO>().ReverseMap();
             CreateMap<ProductPrice, ProductPriceDTO>().ReverseMap();
-
+            CreateMap<OrderHeaderDTO, OrderHeader>().ReverseMap();
+            CreateMap<OrderDetailDTO, OrderDetail>().ReverseMap();
+            CreateMap<OrderDTO, Order>().ReverseMap();
 
         }
     }
